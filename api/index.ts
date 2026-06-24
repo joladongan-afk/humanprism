ee8b073c6235dae7c2332cd62202cd82a3624f2e
joladongan-afk <joladongan@gmail.com>
2026-06-24 16:50:24 +0900

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,10 @@
-import { createServer } from "http";
-import app from "../dist/index.js";
+import express from "express";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+
+const app = express();
+
+app.set("trust proxy", 1);
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 export default app;
