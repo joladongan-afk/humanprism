6d1a878c33c163045ae48174decdd013de69f4d9
joladongan-afk <joladongan@gmail.com>
2026-07-14 09:59:15 +0900

    [temp] Register abTest router for Chapter2 A/B test

--- server/routers.ts
+++ server/routers.ts
@@ -1,4 +1,5 @@
 import { COOKIE_NAME, RECORD_RETENTION_MS, USAGE_WINDOW_MS } from "@shared/const";
+import { abTestRouter } from "./ab-test-router";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { getSessionCookieOptions } from "./_core/cookies";
@@ -1166,5 +1167,6 @@ export const appRouter = router({
   cs: csRouter,
   naming: namingRouter,
   publicStats: publicProcedure.query(() => db.getPublicStats()),
+  abTest: abTestRouter,
 });
 export type AppRouter = typeof appRouter;
